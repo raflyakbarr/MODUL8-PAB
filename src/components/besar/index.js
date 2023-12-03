266daf4ddf1c62bff4a62ac4109b7e95d209b418
raflyakbarr <133252961+raflyakbarr@users.noreply.github.com>
2023-12-03 12:12:19 +0700

    Add files via upload

---
+++ src/components/besar/index.js
@@ -0,0 +1,5 @@
+import BottomNavigator from "./BottomNavigator";
+import ListNote from "./ListNote";
+
+export {BottomNavigator, ListNote}
+
---
+++ src/components/kecil/index.js
@@ -0,0 +1,7 @@
+import Button from './button' 
+import Input from './input'
+import CategoryTab from './categoryTab'
+import Pilihan from './pilihan'
+
+export {Button, Input, CategoryTab, Pilihan}
+
